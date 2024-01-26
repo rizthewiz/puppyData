3907f8b495ee75819b2a6f1b0547cd0d56740f06
rizthewiz <rizthewiz>
2024-01-25 20:17:41 -0500

    this works

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { puppyList } from "./data.js";
 export default function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
+  const [featuredPup, setFeaturedPup] = useState(null);
 
   function handleClick(id) {
     setFeatPupId(id);
+    setFeaturedPup(puppies.find((pup) => pup.id === id));
   }
 
   return (
@@ -21,15 +23,10 @@ export default function App() {
           {puppy.name}
         </p>
       ))}
-      {
-        (featuredPup = featPupId
-          ? puppies.find((pup) => pup.id === featPupId)
-          : null)
-      }
 
       {featuredPup && (
         <div>
-          <h2>{feturedPup.name}</h2>
+          <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
